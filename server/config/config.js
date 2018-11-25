8c9450543c0f089cdc852122301e15ccc3c2ca83
dario zubaray <dzubaray@gmail.com>
2018-11-25 16:39:51 -0300

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev') {
   urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-  urlDB = 'mongodb://cafe-user:q1w2e3r4@ds249325.mlab.com:49325/cafe'
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
