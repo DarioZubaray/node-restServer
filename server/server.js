58ba6d61572d1ff885e6912d5d04804ebe537d53
dario zubaray <dzubaray@gmail.com>
2018-11-25 13:06:01 -0300

    añaiendo mongoose y uniquevalidator, creando el modelo de datos, validando mail y roles

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -9,33 +10,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuario', (req, res) => {
-  res.json('Hola Juli');
-});
+app.use( require('./routes/usuario') );
 
-app.post('/usuario', (req, res) => {
-  let body = req.body;
-  if(body.nombre === undefined){
-        res.status(400).json({
-          ok: false,
-          mensaje: 'el nombre es necesario'
-        });
-  } else {
-    res.json({
-      persona: body
-    });
+mongoose.connect('mongodb://localhost:27017/cafe', (err, resp) => {
+  if(err){
+    throw err;
   }
-});
-
-app.put('/usuario/:id', (req, res) => {
-  let id = req.params.id;
-  res.json({
-    id
-  });
-});
-
-app.delete('/usuario', (req, res) => {
-  res.json('delete usuario');
+  console.log('Base de datos online');
 });
 
 app.listen(process.env.PORT, () => {
