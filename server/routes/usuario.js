a6d2edbea4d0fdab1ab09512a4f9169f76dc644d
dario zubaray <dzubaray@gmail.com>
2018-11-25 14:55:15 -0300

    Implementando el PUT y GET de usuarios

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,11 +1,40 @@
 const express = require('express');
 const Usuario = require('../models/usuario');
 const bcrypt = require('bcrypt');
+const _ = require('underscore');
 
 const app = express();
 
 app.get('/usuario', (req, res) => {
-  res.json('Hola Juli');
+
+  let desde = Number(req.query.desde) || 0;
+  let hasta = Number(req.query.hasta) || 5;
+
+  Usuario.find({}, 'nombre email img role estado google')
+         .skip(desde)
+         .limit(hasta)
+         .exec((err, usuarios) => {
+           if(err){
+             return res.status(400).json({
+               ok: false,
+               err
+             });
+           }
+
+           Usuario.count({}, (err, conteo) => {
+             if(err){
+               return res.status(400).json({
+                 ok: false,
+                 err
+               });
+             }
+             res.json({
+                 ok: true,
+                 conteo,
+                 usuarios
+             });
+           });
+         });
 });
 
 app.post('/usuario', (req, res) => {
@@ -37,8 +66,19 @@ app.post('/usuario', (req, res) => {
 
 app.put('/usuario/:id', (req, res) => {
   let id = req.params.id;
-  res.json({
-    id
+  let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+
+  Usuario.findByIdAndUpdate( id, body, {new: true, runValidators: true}, (err, usuarioDB) => {
+    if(err) {
+      return res.status(400).json({
+        ok: false,
+        err
+      });
+    }
+    res.json({
+      ok: true,
+      usuario: usuarioDB
+    });
   });
 });
 
